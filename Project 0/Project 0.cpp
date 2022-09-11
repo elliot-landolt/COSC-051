3f12238a05dcdd69c7d76a39cd719bd56276f1f4
Elliot <eal133@georgetown.edu>
2022-09-11 17:52:18 -0400

    9-11 Project Completed

--- Project 0/Project 0.cpp
+++ Project 0/Project 0.cpp
@@ -3,5 +3,6 @@
 using namespace std;
 
 int main(){
-    
+    cout << "My name is: Elliot Landolt \nMy student id is: EAL133 \nMy class email is: eal133@georgetown.edu \nI know how to submit assignments!";
+    return 0;
 }
