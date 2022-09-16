bcec18bd39d030b07449d552628598cb1c8d39c8
Elliot <eal133@georgetown.edu>
2022-09-16 15:52:54 -0400

    9-16 Wrote and tested program

--- Project1/project1.cpp
+++ Project1/project1.cpp
@@ -3,13 +3,19 @@
 using namespace std;
 
 int main (){
-    int length, width, sqQuart, costQuart, costTotal;
-    int surfArea = length * width;
-    int totalQuart = surfArea / sqQuart;
-    int costTotal = totalQuart * costQuart;
+    int length, width, sqQuart, costQuart, costTotal, surfArea, totalQuart;
     cout << "How long is the wall? ";
     cin >> length;
     cout << "How wide is the wall? ";
     cin >> width;
-    return 0;
+    cout << "How many square feet does each quart of paint cover? ";
+    cin >> sqQuart;
+    cout << "How much does each quart cost? ";
+    cin >> costQuart;
+    surfArea = length * width;
+    totalQuart = surfArea / sqQuart;
+    costTotal = totalQuart * costQuart;
+    cout << "You will need " << totalQuart << " quarts of paint to cover the wall. It will cost " 
+        << costTotal << " dollars to paint the wall";
+    
 }
