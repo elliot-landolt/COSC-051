6466ee82e2de86115ab71a32f096abf7b1466d02
Elliot Landolt <eal133@georgetown.edu>
2022-09-22 11:12:59 -0400

    Project 2 created 9-22

---
+++ Project2/project2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+    int userInput;
+    cout << "Press 0 for painting a wall, press 1 for ordering food";
+    cin >> userInput;
+    if (userInput == 0){
+        double length, width, sqQuart, costQuart, costTotal, surfArea, totalQuart;
+        cout << "How long is the wall? ";
+        cin >> length;
+        cout << "How wide is the wall? ";
+        cin >> width;
+        cout << "How many square feet does each quart of paint cover? ";
+        cin >> sqQuart;
+        cout << "How much does each quart cost? ";
+        cin >> costQuart;
+        surfArea = length * width;
+        totalQuart = surfArea / sqQuart;
+        costTotal = totalQuart * costQuart;
+        cout << "You will need " << totalQuart << " quarts of paint to cover the wall. It will cost " 
+            << costTotal << " dollars to paint the wall";
+    }
+    if (userInput == 1){
+
+    }
+
+    if (userInput == 2){
+
+    }
+}
