ebfb616ca43380f2787298577d3801d7be53a970
Elliot Landolt <eal133@georgetown.edu>
2022-09-22 11:32:47 -0400

    9-22

--- Test/test.cpp
+++ Test/test.cpp
@@ -4,10 +4,14 @@
 using namespace std;
 
 int main(){
-
+    int num = 5;
     string name;
     cout << "test\n";
     getline(cin, name);
+    if (num == 4 || name == "Elliot"){
+        cout << "it worked";
+        return 0;
+    }
     cout << name;
     cin >> name;
     cout << name;
