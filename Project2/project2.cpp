6be4072f0b7d02c21258ca4fc5a73d3f604dd596
Elliot Landolt <eal133@georgetown.edu>
2022-10-04 12:02:02 -0400

    General Scope Created

--- Project2/project2.cpp
+++ Project2/project2.cpp
@@ -3,43 +3,50 @@
 using namespace std;
 
 int main (){
+    int done = 0;
     int userInput;
-    cout << "Press 1 for painting a wall, Press 2 for mowing the lawn, press 3 for ordering food ";
-    cin >> userInput;
-    switch (userInput)
+    while (done == 0)
     {
-    case 1:
-        double length, width, sqQuart, costQuart, costTotal, surfArea, totalQuart;
-        cout << "How long is the wall? ";
-        cin >> length;
-        cout << "How wide is the wall? ";
-        cin >> width;
-        cout << "How many square feet does each quart of paint cover? ";
-        cin >> sqQuart;
-        cout << "How much does each quart cost? ";
-        cin >> costQuart;
-        surfArea = length * width;
-        totalQuart = surfArea / sqQuart;
-        costTotal = totalQuart * costQuart;
-        cout << "You will need " << totalQuart << " quarts of paint to cover the wall. It will cost " 
-            << costTotal << " dollars to paint the wall";
-        break;
-    
-    case 2:
-
-    case 3: 
-
-    default:
-        if (userInput > 3)
+        cout << "\nPress 1 for painting a wall, Press 2 for mowing the lawn, press 3 for ordering food ";
+        cin >> userInput;
+        switch (userInput)
         {
-            cout << userInput << " ";
-            for (int i = 0; i < userInput; i++)
+        case 0:
+            done = 1;
+            break;
+        case 1:
+            double length, width, sqQuart, costQuart, costTotal, surfArea, totalQuart;
+            cout << "How long is the wall? ";
+            cin >> length;
+            cout << "How wide is the wall? ";
+            cin >> width;
+            cout << "How many square feet does each quart of paint cover? ";
+            cin >> sqQuart;
+            cout << "How much does each quart cost? ";
+            cin >> costQuart;
+            surfArea = length * width;
+            totalQuart = surfArea / sqQuart;
+            costTotal = totalQuart * costQuart;
+            cout << "You will need " << totalQuart << " quarts of paint to cover the wall. It will cost " 
+                << costTotal << " dollars to paint the wall";
+            break;
+    
+        case 2:
+
+        case 3: 
+
+        default:
+            if (userInput > 3)
             {
-                cout << "*";
+                cout << userInput << " ";
+                for (int i = 0; i < userInput; i++)
+                {
+                    cout << "*";
+                }
+                break;
             }
+            else
+                cout << "Please enter a valid number";
         }
-        else
-            cout << "Please enter a valid number";
-        break;
     }
 }
