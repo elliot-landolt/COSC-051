6f02e74184e1ad740f5544ef352e79c449355859
Elliot Landolt <eal133@georgetown.edu>
2022-10-18 11:50:56 -0400

    Added file input/outputs

--- Project2/project2.cpp
+++ Project2/project2.cpp
@@ -1,69 +1,79 @@
 #include <iostream>
 #include <cmath>
+#include <fstream>
 
 using namespace std;
+
+ofstream outFile;
+ifstream inFile;
 
 int main (){
     int done = 0;
     int userInput;
+    inFile.open("input.txt");
+    outFile.open("output.txt");
     while (done == 0)
     {
-        cout << "\nPress 1 for painting a wall, Press 2 for mowing the lawn, press 3 for ordering food ";
-        cin >> userInput;
+        cout << "\nPress 1 for painting a wall, Press 2 for mowing the lawn, press 3 for ordering food " << endl;
+        inFile >> userInput;
         switch (userInput)
         {
         case 0:
             done = 1;
+            inFile.close();
+            outFile.close();
             break;
         case 1:
             double length, width, sqQuart, costQuart, costTotal, surfArea, totalQuart;
             cout << "How long is the wall? ";
-            cin >> length;
+            inFile >> length;
             cout << "How wide is the wall? ";
-            cin >> width;
+            inFile >> width;
             cout << "How many square feet does each quart of paint cover? ";
-            cin >> sqQuart;
+            inFile >> sqQuart;
             cout << "How much does each quart cost? ";
-            cin >> costQuart;
+            inFile >> costQuart;
             surfArea = length * width;
             totalQuart = ceil(surfArea / sqQuart);
             costTotal = totalQuart * costQuart;
-            cout << "You will need " << totalQuart << " quarts of paint to cover the wall. It will cost " 
-                << costTotal << " dollars to paint the wall";
+            outFile << "You will need " << totalQuart << " quarts of paint to cover the wall. It will cost " 
+                << costTotal << " dollars to paint the wall" << endl;
             break;
     
         case 2:
-            double duration, cutRate;
+            double duration, cutRate, minDuration;
             cout << "How long is the lawn? ";
-            cin >> length;
+            inFile >> length;
             cout << "How wide is the lawn? ";
-            cin >> width;
+            inFile >> width;
             cout << "At what rate does the mower cut the lawn in sqft/hr? ";
-            cin >> cutRate;
+            inFile >> cutRate;
             surfArea = length * width;
             duration = surfArea / cutRate;
-            cout << "It will take " << duration << " hours to cut the lawn";
+            minDuration = duration * 60;
+            outFile << "It will take " << minDuration << " minutes to cut the lawn" << endl;
             break;
         case 3: 
             double deliveryLength, milesPer, fuelCost, gallons;
             cout << "How far is the delivery in miles? ";
-            cin >> deliveryLength;
+            inFile >> deliveryLength;
             cout << "How many miles per gallon does the car get? ";
-            cin >> milesPer;
+            inFile >> milesPer;
             cout << "What is the fuel cost per gallon? ";
-            cin >> fuelCost;
+            inFile >> fuelCost;
             gallons = deliveryLength / milesPer;
             costTotal = gallons * fuelCost;
-            cout << "It will cost " << costTotal << " dollars in gas to make the delivery.";
+            outFile << "It will cost " << costTotal << " dollars in gas to make the delivery." << endl;
             break;
         default:
             if (userInput > 3)
             {
-                cout << userInput << " ";
+                outFile << userInput << " ";
                 for (int i = 0; i < userInput; i++)
                 {
-                    cout << "*";
+                    outFile << "*";
                 }
+                outFile << endl;
                 break;
             }
             else
