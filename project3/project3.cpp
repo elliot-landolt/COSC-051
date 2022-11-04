2952d773a2309c4162b6546173d9fbf77bfb2b6a
Elliot Landolt <eal133@georgetown.edu>
2022-11-04 01:17:39 -0400

    Student Database

--- project3/project3.cpp
+++ project3/project3.cpp
@@ -8,6 +8,8 @@ using namespace std;
 
 const int roomCapacity = 45;
 const int numberOfGrades = 7;
+ofstream ofs;
+
 
 struct Student // create student structure
 {
@@ -21,14 +23,21 @@ void weightedAverage (Student&, double[]);
 void grade (Student&);
 vector <Student> readStudentsFromFile(double[]);
 void printStudent (Student&);
+void sortStudent (Student&);
 
 int main (){
     double weights[numberOfGrades] = {0.02, 0.1, 0.1, 0.14, 0.14, 0.2, 0.3}; // create weights array
     Student student;
     vector <Student> students = readStudentsFromFile(weights);
+    ofs.open("P3Output.txt");
     for(Student student : students)
     {
         printStudent(student);
+    }
+    ofs << "Sorted by weighted score: " << endl;
+    for(Student student : students)
+    {
+        sortStudent(student);
     }
 }
 
@@ -41,16 +50,16 @@ void grade (Student& student){
         if (student.weightedScore >= 90){
             student.finalGrade = 65;
         }
-        else if (student.weightedScore >= 80 and student.weightedScore <= 89){
+        else if (student.weightedScore >= 80 and student.weightedScore < 90){
             student.finalGrade = 66;
         }
-        else if(student.weightedScore >= 70 and student.weightedScore <= 79){
+        else if(student.weightedScore >= 70 and student.weightedScore < 80){
             student.finalGrade = 67;
         }
-        else if(student.weightedScore >= 60 and student.weightedScore <= 69){
+        else if(student.weightedScore >= 60 and student.weightedScore < 70){
             student.finalGrade = 68;
         }
-        else if(student.weightedScore >= 0 and student.weightedScore <= 59){
+        else if(student.weightedScore >= 0 and student.weightedScore < 60){
             student.finalGrade = 70;
         }
         else
@@ -61,9 +70,6 @@ void grade (Student& student){
 }
 
 void printStudent (Student& student){
-    ofstream ofs;
-    ofs.open("P3Output.txt");
-
     ofs << student.name << " Grades: ";
 
     for (int i = 0; i< 7; i++){
@@ -72,7 +78,15 @@ void printStudent (Student& student){
 
     ofs << "Weighted Average: " << student.weightedScore;
     ofs << " Final Grade: " << student.finalGrade << endl;
+}
 
+void sortStudent (Student& student){
+    ofs << student.name << " Grades: ";
+    for (int i = 0; i< 7; i++){
+        ofs << student.grades[i] << " ";
+    }
+    ofs << "Weighted Average: " << student.weightedScore;
+    ofs << " Final Grade: " << student.finalGrade << endl;
 }
 
 vector <Student> readStudentsFromFile(double weights[]){
