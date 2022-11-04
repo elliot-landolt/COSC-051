3a93b2e2708d4c4a2ccfdea20f32ea2a148d21ce
Elliot Landolt <eal133@georgetown.edu>
2022-11-03 23:49:52 -0400

    Student Database

--- project3/project3.cpp
+++ project3/project3.cpp
@@ -2,70 +2,93 @@
 #include <cmath>
 #include <string>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
-ofstream ofs;
-ifstream ifs;
 const int roomCapacity = 45;
+const int numberOfGrades = 7;
 
 struct Student // create student structure
 {
     string name;
-    int grades[7]; // create grades array
+    int grades[numberOfGrades]; // create grades array
     float weightedScore;
     char finalGrade;
 };
 
+void weightedAverage (Student&, double[]);
+void grade (Student&);
+vector <Student> readStudentsFromFile(double[]);
+void printStudent (Student&);
+
 int main (){
-    ifs.open("P3Test.txt");
-    ofs.open("P3output.txt");
-    double weights[7] = {0.02, 0.1, 0.1, 0.14, 0.14, 0.2, 0.3}; // create weights array
+    double weights[numberOfGrades] = {0.02, 0.1, 0.1, 0.14, 0.14, 0.2, 0.3}; // create weights array
     Student student;
-
-}
-
-void weightedAverage (Student&student, double weights[]){
-    double avg = 0;
-    for (int i =0; i < 7; i++){
-    avg += student.grades[i] * weights[i];
+    vector <Student> students = readStudentsFromFile(weights);
+    for(Student student : students)
+    {
+        printStudent(student);
     }
 }
 
-void grade (Student&student){
+void grade (Student& student){
+    ofstream ofs;
+    ofs.open("P3Output.txt");
+
     for (int i = 0; i < roomCapacity; i++)
     {
         if (student.weightedScore >= 90){
-         student.finalGrade = 65;
-    }
-    else if (student.weightedScore >= 80 and student.weightedScore <= 89){
-        student.finalGrade = 66;
-    }
-    else if(student.weightedScore >= 70 and student.weightedScore <= 79){
-        student.finalGrade = 67;
-    }
-    else if(student.weightedScore >= 60 and student.weightedScore <= 69){
-        student.finalGrade = 68;
-    }
-    else if(student.weightedScore >= 0 and student.weightedScore <= 59){
-        student.finalGrade = 70;
-    }
-    else
-    {
-        ofs << "Invalid Score";
-    }
+            student.finalGrade = 65;
+        }
+        else if (student.weightedScore >= 80 and student.weightedScore <= 89){
+            student.finalGrade = 66;
+        }
+        else if(student.weightedScore >= 70 and student.weightedScore <= 79){
+            student.finalGrade = 67;
+        }
+        else if(student.weightedScore >= 60 and student.weightedScore <= 69){
+            student.finalGrade = 68;
+        }
+        else if(student.weightedScore >= 0 and student.weightedScore <= 59){
+            student.finalGrade = 70;
+        }
+        else
+        {
+            ofs << "Invalid Score";
+        }
     } 
 }
 
+void printStudent (Student& student){
+    ofstream ofs;
+    ofs.open("P3Output.txt");
+
+    ofs << student.name << " Grades: ";
+
+    for (int i = 0; i< 7; i++){
+        ofs << student.grades[i] << " ";
+    }
+
+    ofs << "Weighted Average: " << student.weightedScore;
+    ofs << " Final Grade: " << student.finalGrade << endl;
+
+}
+
 vector <Student> readStudentsFromFile(double weights[]){
+    ifstream ifs;
+    ifs.open("P3Test.txt");
+
     vector <Student> students;
-    string name;
+    string name, input;
+    
     while (ifs >> name) {
         Student student;
         student.name = name;
         for (int i = 0; i < 7; i++)
         {
-            ifs >> student.grades[i];
+            ifs >> input;
+            student.grades[i] = stoi(input);
         }
         weightedAverage(student, weights);
         grade(student);
@@ -74,12 +97,10 @@ vector <Student> readStudentsFromFile(double weights[]){
     return students;
 }
 
-void printStudent (Student&student){
-    ofs << student.name << "Grades: ";
-    for (int grade : student.grades){
-        ofs << grade << " ";    
+void weightedAverage (Student&student, double weights[]){
+    double avg = 0;
+    for (int i =0; i < numberOfGrades; i++){
+    avg += (student.grades[i] * weights[i]);
     }
-    ofs << "Weighted Average: " << student.weightedScore;
-    ofs << "Final Grade: " << student.finalGrade << endl;
-
+    student.weightedScore = avg;
 }
